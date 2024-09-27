c408ba492102e0def36c4c8d43b3f12a8c8a609c
somasekhar18005 <154443222+somasekhar18005@users.noreply.github.com>
2024-09-27 14:18:26 +0530

    A Small basic intro.

---
+++ app.js
@@ -0,0 +1,7 @@
+const math=require("./math");
+
+console.log(math.add(10,20));
+console.log(math.multipy(2,50));
+
+
+
---
+++ math.js
@@ -0,0 +1,14 @@
+module.exports={
+add :function (a,b){
+    return a+b;
+},
+
+multipy:function (a,b){
+    return a*b;
+}
+
+
+
+
+};
+
